e2b580c995ffd64c1a7d388d3d01d4d1d25c8a7a
Mateusz <bialek328@gmail.com>
2024-05-14 22:17:45 +0200

    calculator functionality implemented

--- main/main.go
+++ main/main.go
@@ -3,31 +3,53 @@ package main
 import (
 	"errors"
 	"fmt"
+
+	"example.com/calculations"
 )
 
-var operators = []string{"+", "-", "*", "/"}
-
-func validateOperator(input string) (string, error) {
-	for _, opr := range operators {
-		if input == opr {
-			return opr, nil
-		}
-	}
-	return "", errors.New("invalid operator!")
-}
-
-func getUserInput() string {
-	fmt.Println("Input: ")
+func getOperator() string {
+	fmt.Println("Operator: ")
 	input := ""
 	fmt.Scanln(&input)
 	return input
 }
 
-func main() {
-	str := getUserInput()
-	opr, err := validateOperator(str)
-	if err != nil {
-		fmt.Printf("Error: ", err)
+func getNumbers() (float32, float32) {
+	var number1, number2 float32
+	fmt.Println("First number:")
+	fmt.Scanln(&number1)
+	fmt.Println("Second number:")
+	fmt.Scanln(&number2)
+	return number1, number2
+}
+
+func handleCalculation(opr string, number1, number2 float32) (float32, error) {
+	switch opr {
+	case "+":
+		return calculations.Add(number1, number2), nil
+
+	case "-":
+		return calculations.Substract(number1, number2), nil
+
+	case "*":
+		return calculations.Multiply(number1, number2), nil
+
+	case "/":
+		return calculations.Divide(number1, number2)
+
+	default:
+		return 0, errors.New("invalid operator")
 	}
-	if 
+}
+
+func main() {
+	opr := getOperator()
+	// opr, err := validateOperator(str)
+	number1, number2 := getNumbers()
+	result, err := handleCalculation(opr, number1, number2)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	} else {
+		fmt.Printf("Result: %f\n", result)
+	}
 }
